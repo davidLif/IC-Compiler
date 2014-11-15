add159be89422443ccc16d3ca7cee990b77ceb04
shacharpooyae <shacharpooyae@gmail.com>
2014-11-15 13:54:59 +0200

    gps project

---
+++ sadna/GPSAppWindowsPhone/GPSAppWindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Geolocation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace GPSAppWindowsPhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Geolocator geolocator = null;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            geolocator = new Geolocator();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String latitude;
+            String longtitude;
+            try
+            {
+ 
+                Geoposition position = await geolocator.GetGeopositionAsync();
+
+                latitude = position.Coordinate.Point.Position.Latitude.ToString();
+                longtitude = position.Coordinate.Point.Position.Longitude.ToString();
+                
+                /* printing data in the label */
+                pos.Text = "Latitude: " + latitude + "\nLongtitude: " + longtitude;
+
+             
+            }
+            catch (Exception )
+            {
+                pos.Text = "Could not get the desired data.";
+            }
+        }
+    }
+}
